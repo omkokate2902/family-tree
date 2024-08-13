e59fe376abfac0ee6469d054fc1256d931c4634b
Om Kokate <om.kokate2902@gmail.com>
2024-08-14 04:35:33 +0530

    Initial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Testing branch works');
+    res.send('Production branch works');
 });
 
 const PORT = process.env.PORT || 3000;
