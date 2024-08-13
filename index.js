c706d65ab985b2a7bd9cb4b87aac1a0ec2909b1a
Om Kokate <om.kokate2902@gmail.com>
2024-08-14 04:24:40 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World! (test world 4)');
+    res.send('Testing branch works');
 });
 
 const PORT = process.env.PORT || 3000;
