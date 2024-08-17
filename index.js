785f6f63b1ef9a4988d09408ff3e7bf96d43d48e
Om Kokate <om.kokate2902@gmail.com>
2024-08-17 12:08:28 +0530

    demo2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Testing branch works fine. demo');
+    res.send('Testing branch works fine. demo2');
 });
 
 const PORT = process.env.PORT || 3000;
