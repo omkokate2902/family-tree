74942c264acd7bcb672eab8c797febe33caf6217
Om Kokate <om.kokate2902@gmail.com>
2024-08-17 11:27:21 +0530

    jaadu

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Testing branch works');
+    res.send('Testing branch works fine');
 });
 
 const PORT = process.env.PORT || 3000;
