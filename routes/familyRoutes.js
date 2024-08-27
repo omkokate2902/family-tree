304d1187de23cf58eeca6b7dfd1ad30d832eca17
Shifan2077 <nagarjishifan@gmail.com>
2024-08-27 13:50:53 +0530

    delete-patch

--- routes/familyRoutes.js
+++ routes/familyRoutes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { addFamily, getFamilyByUserId } = require('../controllers/familyController');
+const { deleteFamilyMember, deleteChild } = require('../controllers/deleteChil'); // Importing delete function
+
+const { updateChild } = require('../controllers/patchChild');
 
 const router = express.Router();
 
@@ -8,5 +11,13 @@ router.post('/families', addFamily);
 
 // Route to get family data by user_id
 router.get('/families/:user_id', getFamilyByUserId);
+
+// Route to delete a family member/child member 
+router.delete('/families/delete-child', deleteChild);
+
+// Route to update a family member/child member
+router.patch('/families/updateChild', updateChild);
+
+
 
 module.exports = router;
